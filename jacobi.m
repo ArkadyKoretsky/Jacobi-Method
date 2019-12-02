3534f8df479109700956ed737db54ae758315ab7
Arkady Koretsky <arkady1992@gmail.com>
2019-12-03 00:22:48 +0200

    Added the conditions checking for the method

--- jacobi.m
+++ jacobi.m
@@ -1,16 +1,33 @@
 clc
 clear all
-
+    
 vectorDimension = input('Enter Vector Diomnesion: ');
 matrixDimension = vectorDimension * vectorDimension;
 
-prompt = sprintf('Enter %d values in [] for vector b: ', vectorDimension);
+prompt = sprintf("Enter %d values in [] for vector b: ", vectorDimension);
 bVector = input(prompt);
 bVector = reshape(bVector,[vectorDimension,1]);
 
-prompt = sprintf('Enter %d values in [] for Matrix A, each row separete with semicolon (;): ', matrixDimension);
+prompt = sprintf("Enter %d values in [] for Matrix A, each row separete with semicolon (;): ", matrixDimension);
 A_Matrix = input(prompt);
 
 if det(A_Matrix) == 0
-    disp('There is no solution for the current equation'); 
+    msgbox("There isn't exactly one solution for the current problem" + newline + "Program will exit now");
+    exit
 end
+
+if all((2*abs(diag(A))) >= sum(abs(A),2)) % check diagonally dominant matrix, if true perform the Jacobi method
+    % TODO: implement Jacobi function
+else
+  D_Matrix = diag(diag(A_matrix));
+  L_Matrix = tril(A_MAtrix) - D_Matrix;
+  U_Matrix = triU(A_MAtrix) - D_Matrix;
+  Bj_Matrix = -inv(D_Matrix) * (L_Matrix + U_Matrix);
+
+  if all(abs(eig(Bj_Matrix)) < 1) % check if all the eigen values of Bj are inside the unit circle, if true perform the Jacobi method
+    % TODO: implement Jacobi function 
+  else
+     msgbox("This method won't converge for the A matrix and b vector" + newline + "We suggest you to try the Gauss–Seidel method"); 
+  end
+end
+
