578ff202f7a0974773329382416197ffdbb88e1b
Arkady Koretsky <arkady.koretsky@intel.com>
2019-12-02 17:54:22 +0200

    Started the equation inputs

--- jacobi.m
+++ jacobi.m
@@ -1,2 +1,16 @@
 clc
 clear all
+
+vectorDimension = input('Enter Vector Diomnesion: ');
+matrixDimension = vectorDimension * vectorDimension;
+
+prompt = sprintf('Enter %d values in [] for vector b: ', vectorDimension);
+bVector = input(prompt);
+bVector = reshape(bVector,[vectorDimension,1]);
+
+prompt = sprintf('Enter %d values in [] for Matrix A, each row separete with semicolon (;): ', matrixDimension);
+A_Matrix = input(prompt);
+
+if det(A_Matrix) == 0
+    disp('There is no solution for the current equation'); 
+end
