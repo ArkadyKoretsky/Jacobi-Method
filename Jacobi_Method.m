b935c0c4c24546b4ff5b8ebfd68de8c00356bf71
Arkady Koretsky <arkady.koretsky@intel.com>
2019-12-03 20:50:24 +0200

    Added the epsilon check within vectors

--- Jacobi_Method.m
+++ Jacobi_Method.m
@@ -2,13 +2,15 @@ function [xVector] = Jacobi_Method(A_Matrix, bVector)
 %JACOBI_METHOD Summary of this function goes here
 %   Detailed explanation goes here
 [row,col] = size(A_Matrix);
-xVector = zeros(row,1);
-error = 1e-5;
+xVector = rand([row,1]);
+tempVector = rand([row,1]);
+epsilon = 1e-7;
 
-
-for i=1:col
-   tempVector = xVector;
-   xVector(i) = (bVector(i) - (A_Matrix(i,1:i-1)*tempVector(1:i-1))-(A_Matrix(i,i+1:col)*tempVector(i+1:col)))/(A(row,row));
+while max(abs(xVector - tempVector)) < epsilon
+    for i=1:col
+        tempVector = xVector;
+        xVector(i) = (bVector(i) - (A_Matrix(i,1:i-1)*tempVector(1:i-1))-(A_Matrix(i,i+1:col)*tempVector(i+1:col)))/(A(row,row));
+    end
 end
 
 end
