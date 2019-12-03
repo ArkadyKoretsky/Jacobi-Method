cf26f50e1c248509aa5498201e42a74f565d60e9
Arkady Koretsky <arkady1992@gmail.com>
2019-12-04 00:35:05 +0200

    Hopefully Finished

--- Jacobi_Method.m
+++ Jacobi_Method.m
@@ -1,17 +1,21 @@
 function [xVector] = Jacobi_Method(A_Matrix, bVector)
-%JACOBI_METHOD Summary of this function goes here
-%   Detailed explanation goes here
+
 [row,col] = size(A_Matrix);
 xVector = rand([row,1]);
-tempVector = rand([row,1]);
-epsilon = 1e-7;
+epsilon = 1e-7; % ? = 10^-7
 
-while max(abs(xVector - tempVector)) < epsilon
+% first iteration of the Jacobi method outside the while loop to intialize
+% the temp vector
+for i=1:row
+    tempVector = xVector;
+    xVector(i) = (bVector(i) - (A_Matrix(i,:) * tempVector - A_Matrix(i,i) * tempVector(i))) / A_Matrix(i,i);
+end
+
+while max(abs(xVector - tempVector)) >= epsilon
     for i=1:col
         tempVector = xVector;
-        xVector(i) = (bVector(i) - (A_Matrix(i,1:i-1)*tempVector(1:i-1))-(A_Matrix(i,i+1:col)*tempVector(i+1:col)))/(A(row,row));
+        xVector(i) = (bVector(i) - (A_Matrix(i,:) * tempVector - A_Matrix(i,i) * tempVector(i))) / A_Matrix(i,i);
     end
 end
 
 end
-
--- jacobi.m
+++ jacobi.m
@@ -1,35 +1,40 @@
 clc
 clear all
     
+A_Matrix = input("Enter values in [] for square matrix A, each row separete with semicolon (;):" + newline);
+[row,col] = size(A_Matrix);
 
-
-prompt = sprintf("Enter values in [] for vector b: ");
+prompt = sprintf("Enter %d values in [] for vector b: ", row);
 bVector = input(prompt);
-[row,col] = size(bVector);
-vectorDimension = row * col; % number of elements * 1
-bVector = reshape(bVector,[vectorDimension,1]);
-
-matrixDimension = vectorDimension * vectorDimension;
-prompt = sprintf("Enter %d values in [] for square matrix A, each row separete with semicolon (;): ", matrixDimension);
-A_Matrix = input(prompt);
+bVector = reshape(bVector,[row,1]);
 
 if det(A_Matrix) == 0
     msgbox("This Problem don't have exactly one solution");
-    exit
+    return
 end
 
-if all((2*abs(diag(A))) >= sum(abs(A),2)) % check diagonally dominant matrix, if true perform the Jacobi method
-    % TODO: implement Jacobi function
+if all((2*abs(diag(A_Matrix))) >= sum(abs(A_Matrix),2)) % check diagonally dominant matrix, if true perform the Jacobi method
+    [xVector] = Jacobi_Method(A_Matrix, bVector);
+    prompt = "This system converge with sufficient condition" + newline; 
 else
-  D_Matrix = diag(diag(A_matrix));
-  L_Matrix = tril(A_MAtrix) - D_Matrix;
-  U_Matrix = triU(A_MAtrix) - D_Matrix;
-  Bj_Matrix = -inv(D_Matrix) * (L_Matrix + U_Matrix);
-
+  D_Matrix = diag(diag(A_Matrix));
+  L_Matrix = tril(A_Matrix) - D_Matrix;
+  U_Matrix = triu(A_Matrix) - D_Matrix;
+  Bj_Matrix = -inv(D_Matrix) * (L_Matrix + U_Matrix); 
+  
   if all(abs(eig(Bj_Matrix)) < 1) % check if all the eigen values of Bj are inside the unit circle, if true perform the Jacobi method
-    % TODO: implement Jacobi function 
+    [xVector] = Jacobi_Method(A_Matrix, bVector);
+    prompt = "This system converge with necessary and sufficient condition" + newline;
   else
-     msgbox("This method won't converge for the A matrix and b vector" + newline + "We suggest you to try the Gauss–Seidel method"); 
+     msgbox("This method won't converge for the current A matrix and b vector" + newline + "We suggest you to try the Gauss–Seidel method");
+     return
   end
 end
 
+prompt = prompt + "The answer for this linear system is:" + newline;
+for i=1:row-1
+    prompt = prompt + sprintf("X%d = %g, ", i, xVector(i)); 
+end
+prompt = prompt + sprintf("X%d = %g", row, xVector(row));
+
+msgbox(prompt);
